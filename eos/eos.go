fddcf166ad01235a0b309b81476893f5a01e3699
Vladimir Evgrafov <evgrafov.vladimir@gmail.com>
2018-07-13 17:38:16 +0300

    MUL-1620: balance tracking & stuff
    * get new blocks using p2p protocol
    * parse actions' data for system contracts actions
    * EventAccountCheck checks if account exist (from @vadimicus flow)
    * getRAMPrice get accurate ram price using rammarket

--- eos/eos.go
+++ eos/eos.go
@@ -7,26 +7,34 @@
 package eos
 
 import (
-	"github.com/eoscanada/eos-go"
-	"github.com/eoscanada/eos-go/system"
-	"fmt"
-	"github.com/eoscanada/eos-go/ecc"
-	pb "github.com/Multy-io/Multy-EOS-node-service/proto"
 	"context"
 	"encoding/hex"
+	"fmt"
+	pb "github.com/Multy-io/Multy-EOS-node-service/proto"
+	"github.com/eoscanada/eos-go"
+	"github.com/eoscanada/eos-go/ecc"
+	"github.com/eoscanada/eos-go/system"
+	_ "github.com/eoscanada/eos-go/token"
 )
 
+const MESSAGE_BUFFER_SIZE = 100
+
 type Server struct {
-	Api           *eos.API
-	account       eos.AccountName
+	Api       *eos.API
+	account   eos.AccountName
 	activeKey string
+
+	tracked          map[string]bool // accounts to track
+	balanceChangedCh chan string
 }
 
-func NewServer(url, account, privKey string) (*Server, error) {
+func NewServer(rpcAddr, p2pAddr, account, privKey string, startBlock uint32) (*Server, error) {
 	client := &Server{
-		Api:           eos.New(url),
-		account:       eos.AccountName(account),
-		activeKey: privKey,
+		Api:              eos.New(rpcAddr),
+		account:          eos.AccountName(account),
+		activeKey:        privKey,
+		tracked:          make(map[string]bool),
+		balanceChangedCh: make(chan string, MESSAGE_BUFFER_SIZE),
 	}
 	keyBag := eos.NewKeyBag()
 	err := keyBag.ImportPrivateKey(privKey)
@@ -34,19 +42,21 @@ func NewServer(url, account, privKey string) (*Server, error) {
 		return nil, err
 	}
 	client.Api.SetSigner(keyBag)
+	go client.BlockProcess(context.TODO(), p2pAddr, startBlock)
+
 	return client, nil
 }
 
-func (c *Server) EventGetChainInfo (ctx context.Context, _ *pb.Empty) (*pb.ChainInfo, error) {
+func (c *Server) EventGetChainInfo(ctx context.Context, _ *pb.Empty) (*pb.ChainInfo, error) {
 	resp, err := c.Api.GetInfo()
 	if err != nil {
 		return nil, err
 	}
 	return &pb.ChainInfo{
-		HeadBlockNum:resp.HeadBlockNum,
-		HeadBlockId: hex.EncodeToString(resp.HeadBlockID),
-		HeadBlockTime:resp.HeadBlockTime.Unix(),
-		LastIrreversibleBlockNum:resp.LastIrreversibleBlockNum,
+		HeadBlockNum:             resp.HeadBlockNum,
+		HeadBlockId:              hex.EncodeToString(resp.HeadBlockID),
+		HeadBlockTime:            resp.HeadBlockTime.Unix(),
+		LastIrreversibleBlockNum: resp.LastIrreversibleBlockNum,
 	}, nil
 }
 
@@ -62,7 +72,7 @@ func (c *Server) EventGetBalance(ctx context.Context, req *pb.BalanceReq) (*pb.B
 		return nil, err
 	}
 	balances := &pb.Balances{
-		Assets:make([]*pb.Asset, len(resp)),
+		Assets: make([]*pb.Asset, len(resp)),
 	}
 	for i, a := range resp {
 		balances.Assets[i] = asset(a)
@@ -84,9 +94,9 @@ func (c *Server) EventGetAccount(ctx context.Context, req *pb.Account) (*pb.Acco
 		return nil, err
 	}
 	return &pb.AccountInfo{
-		Name:string(resp.AccountName),
+		Name:              string(resp.AccountName),
 		CoreLiquidBalance: asset(resp.CoreLiquidBalance),
-		RamAvailable: resp.RAMQuota - resp.RAMUsage,
+		RamAvailable:      resp.RAMQuota - resp.RAMUsage,
 	}, nil
 }
 
@@ -100,19 +110,18 @@ func (c *Server) EventPushTransaction(ctx context.Context, req *pb.Transaction) 
 		signatures = append(signatures, txSig)
 	}
 	tx := eos.PackedTransaction{
-		Signatures: signatures,
-		Compression:eos.CompressionType(req.Compression),
-		PackedContextFreeData:req.PackedContextFreeData,
-		PackedTransaction:req.PackedTrx,
+		Signatures:            signatures,
+		Compression:           eos.CompressionType(req.Compression),
+		PackedContextFreeData: req.PackedContextFreeData,
+		PackedTransaction:     req.PackedTrx,
 	}
 	resp, err := c.Api.PushTransaction(&tx)
 	if err != nil {
-		return &pb.PushTransactionResp{
-		}, nil
+		return &pb.PushTransactionResp{}, nil
 	}
 	return &pb.PushTransactionResp{
-		Id:resp.TransactionID,
-		StatusCode:resp.StatusCode,
+		Id:         resp.TransactionID,
+		StatusCode: resp.StatusCode,
 	}, nil
 }
 
@@ -122,27 +131,26 @@ func (c *Server) EventGetTransactionInfo(ctx context.Context, req *pb.Transactio
 		return nil, err
 	}
 	return &pb.TransactionInfo{
-		Id:hex.EncodeToString(resp.ID),
-		BlockNum:resp.BlockNum,
+		Id:       hex.EncodeToString(resp.ID),
+		BlockNum: resp.BlockNum,
 	}, nil
 }
 
-func (c *Server) EventAccountCreate(ctx context.Context, req *pb.AccountCreateReq) (*pb.OkErrResponse, error){
+func (c *Server) EventAccountCreate(ctx context.Context, req *pb.AccountCreateReq) (*pb.OkErrResponse, error) {
 	ownerKey, err := ecc.NewPublicKey(req.OwnerKey)
 	if err != nil {
 		return &pb.OkErrResponse{
-			Ok: false,
+			Ok:    false,
 			Error: fmt.Sprintf("create account: owner key: %s", err),
 		}, nil
 	}
 	activeKey, err := ecc.NewPublicKey(req.OwnerKey)
 	if err != nil {
 		return &pb.OkErrResponse{
-			Ok: false,
+			Ok:    false,
 			Error: fmt.Sprintf("create account: active key: %s", err),
 		}, nil
 	}
-
 	newAcc := system.NewCustomNewAccount(
 		c.account,
 		eos.AccountName(req.Name),
@@ -151,7 +159,7 @@ func (c *Server) EventAccountCreate(ctx context.Context, req *pb.AccountCreateRe
 			Keys: []eos.KeyWeight{
 				{
 					PublicKey: ownerKey,
-					Weight: 1,
+					Weight:    1,
 				},
 			},
 		},
@@ -160,7 +168,7 @@ func (c *Server) EventAccountCreate(ctx context.Context, req *pb.AccountCreateRe
 			Keys: []eos.KeyWeight{
 				{
 					PublicKey: activeKey,
-					Weight: 1,
+					Weight:    1,
 				},
 			},
 		},
@@ -172,12 +180,50 @@ func (c *Server) EventAccountCreate(ctx context.Context, req *pb.AccountCreateRe
 	_, err = c.Api.SignPushActions(newAcc, buyRAM)
 	if err != nil {
 		return &pb.OkErrResponse{
-			Ok:false,
-			Error:fmt.Sprintf("create account: push tx: %s", err),
+			Ok:    false,
+			Error: fmt.Sprintf("create account: push tx: %s", err),
 		}, nil
 	}
 	return &pb.OkErrResponse{
-		Ok:true,
+		Ok: true,
 	}, nil
 }
 
+func (s *Server) EventAccountCheck(ctx context.Context, req *pb.Account) (*pb.Exist, error) {
+	_, err := s.Api.GetAccount(eos.AN(req.Name))
+	return &pb.Exist{
+		Exist: err == nil,
+	}, nil
+}
+
+func (s *Server) BalanceChanged(_ *pb.Empty, stream pb.NodeCommunications_BalanceChangedServer) error {
+	for {
+		select {
+		case account := <-s.balanceChangedCh:
+			balResp, err := s.EventGetBalance(context.TODO(), &pb.BalanceReq{Name: account})
+			if err != nil {
+				return err
+			}
+			ramResp, err := s.EventGetAccount(context.TODO(), &pb.Account{Name: account})
+			balResp.Assets = append(balResp.Assets, &pb.Asset{Precision: 0, Symbol: "RAM", Ammount: ramResp.RamAvailable})
+			stream.Send(balResp)
+		}
+	}
+	return nil
+}
+
+func (s *Server) EventTrackAccount(ctx context.Context, req *pb.Account) (*pb.Empty, error) {
+	s.tracked[req.Name] = true
+	return &pb.Empty{}, nil
+}
+
+func (s *Server) EventSetTrackedAccounts(ctx context.Context, req *pb.Accounts) (*pb.Empty, error) {
+	new := make(map[string]bool)
+	for _, acc := range req.Accounts {
+		new[acc.Name] = true
+	}
+	s.tracked = new
+	return &pb.Empty{}, nil
+
+	return &pb.Empty{}, nil
+}
---
+++ eos/ramprice.go
@@ -0,0 +1,55 @@
+package eos
+
+import (
+	"github.com/eoscanada/eos-go"
+	"fmt"
+	"encoding/json"
+	"strconv"
+)
+
+type RamMarket struct {
+	Supply *eos.Asset `json:"supply"`
+	Base  *BalanceWeight `json:"base"`
+	Quote  *BalanceWeight `json:"quote"`
+}
+
+type BalanceWeight struct {
+	Balance *eos.Asset `json:"balance"`
+	Weight WeightType `json:"weight"`
+}
+
+type WeightType float64
+
+func (w *WeightType) UnmarshalJSON(data []byte) error {
+	var s string
+	json.Unmarshal(data, &s)
+	f, err := strconv.ParseFloat(s, 64)
+	if err != nil {
+		return err
+	}
+	*w = WeightType(f)
+	return nil
+}
+
+// getRAMPrice gets amount of RAM that you can buy for 1 EOS
+func getRAMPrice(api *eos.API) (float64, error) {
+	rawResp, err := api.GetTableRows(eos.GetTableRowsRequest{
+		Code:  "eosio",
+		Scope: "eosio",
+		Table: "rammarket",
+		JSON:  true,
+	})
+	if err != nil {
+		return 0, err
+	}
+	resps := make([]*RamMarket, 1)
+	err = rawResp.JSONToStructs(&resps)
+	if err != nil {
+		return 0, fmt.Errorf("unmarshall %s", err)
+	}
+	market := resps[0]
+
+	price := (10000.0 - ((10000.0 + 199.0) / 200.0)) / (float64(market.Quote.Balance.Amount) / float64(market.Base.Balance.Amount))
+
+	return price, nil
+}
